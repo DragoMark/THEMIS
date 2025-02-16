b051674ceaaab6e18cfaa417170c0b1ee540748b
DragoMark <nihalshettybk@gmail.com>
2025-02-16 14:24:40 +0530

    Fix vite.config bug

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vite.dev/config/
 // export default defineConfig(({ command }) => ({
 export default defineConfig({
-  base: 'THEMIS/',
+  base: '/THEMIS/',
   // base: command == 'build' ? '/themis/' : '/',
   plugins: [react(), tsconfigPaths()],
   css: {
