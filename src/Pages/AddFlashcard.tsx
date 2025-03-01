290ad4278209a3f3ce652bee1317270d21a5aa0d
DragoMark <nihalshettybk@gmail.com>
2025-03-01 17:48:56 +0530

    Add react-hook-form useForm hook

--- src/Pages/AddFlashcard.tsx
+++ src/Pages/AddFlashcard.tsx
@@ -1,40 +1,48 @@
 import { useNavigate } from "react-router-dom";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 import FloatingButton from '@/components/FloatingButton.tsx'
 import { leftArrow } from '@/utils/Characters.ts'
-// import { Flashcard } from '@/utils/Flashcard'
+import { Flashcard } from '@/utils/Flashcard'
 
 // import {saveSingleFlashcard } from '@/utils/localStorage.ts'
-import { FormEvent } from "react";
 
 const AddFlashcard = () => {
     const navigate = useNavigate();
+    // Create a form using react-hook-form  
+    const {
+        // register,
+        handleSubmit,
+        // formState: { errors },
+        watch,
+        // getValues,
+        // setValue,
+        // reset,
+        // clearErrors,
+        // trigger
+    } = useForm<Flashcard>();
 
     const goBack = () => {
         navigate(-1);
     }
 
-    const handleSubmit  = (data: FormEvent) => {
-        // Add the logic here to update localStorage
-        console.log("Form submission data: ", data);
-        // saveSingleFlashcard(data);
+    const onSubmit: SubmitHandler<Flashcard> = (data) => {
+        console.log(data);
+    };
 
-
-    }
+    console.log(watch("name"));
 
     return (
-        <div className="p-4 flex flex-col gap-4 overflow-y-scroll">
+        <div className="p-4 flex flex-col gap-8 overflow-y-scroll">
             <h1 className="font-roboto-mono text-xl p-2 text-center">
                 New Flashcard
             </h1>
 
             <div className="px-2 grid gap-2">
                 {/* // Add the form here where it takes in data for fields that correspond to the data in flashcardStore.ts */}
-                <form onSubmit={handleSubmit}>
-                    <input type="text" />
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+                    
                 </form>
-
-
             </div>
 
             <FloatingButton 
